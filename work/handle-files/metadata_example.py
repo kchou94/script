e7c6ef3d0198eddd1620d918a7a558f49253673d
Kyle Chou <kchou@Kyles-MacBook-Pro.local>
2021-11-05 15:01:12 +0800

    update

---
+++ work/handle-files/metadata_example.py
@@ -0,0 +1,16 @@
+import PyPDF2
+
+
+def main():
+    file_name = '/Users/kchou/Downloads/script/work/handle-files/Google.pdf'
+    pdfFile = PyPDF2.PdfFileReader(open(file_name, 'rb'))
+    pdf_data = pdfFile.getDocumentInfo()
+
+    print("----Metadata of the file----")
+
+    for md in pdf_data:
+        print(md + ": " + pdf_data[md])
+
+
+if __name__ == '__main__':
+    main()
